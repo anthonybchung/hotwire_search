40e13f1b203db02b6612c599000a598cb1d9e115
abchung <anthony.b.chung@gmail.com>
2023-06-04 21:50:23 +1000

    styling table

--- app/javascript/controllers/projects_controller.js
+++ app/javascript/controllers/projects_controller.js
@@ -6,12 +6,28 @@ export default class extends Controller {
   static targets = ["card","searchField"]
   connect() {
   }
+
+  initialize() {
+    this.cardTargets.forEach((card,index)=>{
+      if(index %2 == 0)
+      card.classList.add("bg-violet-100")
+    })
+  }
+
   search(){
     let searchField = this.searchFieldTarget.value.toLowerCase()
+    let backgroundColor = true
     this.cardTargets.forEach((card)=>{
-      let name = card.firstElementChild.innerHTML.slice(5).toLowerCase()
+      let name = card.firstElementChild.innerHTML.toLowerCase()
+      card.classList.remove("bg-violet-100")
       if(name.includes(searchField)){
         card.classList.remove("hidden")
+        if(backgroundColor){
+          card.classList.add("bg-violet-100")
+          backgroundColor = !backgroundColor
+        } else {
+          backgroundColor = !backgroundColor
+        }
       }else{
         card.classList.add("hidden")
       }
